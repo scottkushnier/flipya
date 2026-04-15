3d7925f2af0327d18208f25055fcd90051a196e5
Scott Kushnier <kushnier@hstreet.com>
2026-04-15 07:49:10 -0400

    added different background color for 'development'

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// different background to signify development
+if (import.meta.env.VITE_REACT_APP_ENV == "development") {
+  document.body.classList.add("dev");
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
